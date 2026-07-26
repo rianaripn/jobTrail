2f51db3581f9e4c56e02dc86e77f369d6a055a88
Rian Arip Nugraha <rianarip4@gmail.com>
2026-07-27 05:48:50 +0700

    feat: connect express to postgresql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import pool from './config/db.js'
 
 const app = express()
+app.use(express.json())
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.log('Database connection failed:', err)
+    } else {
+        console.log('Database connected at:', res.rows[0].now)
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.json({ message: 'Server berjalan!' })
