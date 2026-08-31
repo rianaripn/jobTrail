c64aacb1feca014fd3228708294678484378d80a
rianaripnugraha <rianarip4@gmail.com>
2026-09-01 01:20:19 +0700

    feat: implement auth register and login

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import pool from './config/db.js'
+import authRouter from './routes/auth.js'
 
 const app = express()
 app.use(express.json())
+
+app.use('/auth', authRouter)
 
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
